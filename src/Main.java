e7dbf7e4fa051c401570ceb2565d084426c87a26
jaka junaedi <jaka.junaedi03@gmail.com>
2020-07-31 17:41:46 +0700

    commit ke empat

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Mahasiswa mhs = new Mahasiswa();
         mhs.setNama("JAKA");
-
         mhs.setAlamat("jakarta");
         mhs.setUmur(19);
         mhs.tampil();
