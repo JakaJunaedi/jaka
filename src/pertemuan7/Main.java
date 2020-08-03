126e9a6a83ef4be9f8b0cbca58adb13af7932a76
jaka junaedi <jaka.junaedi03@gmail.com>
2020-08-03 16:57:15 +0700

    pertemuan7 penggunaan if dan else if

---
+++ src/pertemuan7/Main.java
@@ -0,0 +1,17 @@
+package pertemuan7;
+
+public class Main {
+    public static void main(String[] args) {
+        int angka = 4;
+
+        if(angka == 1) {
+            System.out.println("angka 1 yang tampil");
+        }else if(angka == 2) {
+            System.out.println("angka 2 yang tampil");
+        }else if(angka == 3) {
+            System.out.println("angka 3 yang tampil");
+        }else {
+            System.out.println("Tidak ada yang bisa tampil");
+        }
+    }
+}
