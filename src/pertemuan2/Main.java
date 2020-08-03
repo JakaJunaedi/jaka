495b8ec994c1ed0b6be16b8ae3253cedd2e8b7e1
jaka junaedi <jaka.junaedi03@gmail.com>
2020-08-03 10:42:15 +0700

    pertemuan 2 belajar variable dengan main class

---
+++ src/pertemuan2/Main.java
@@ -0,0 +1,13 @@
+package pertemuan2;
+
+public class Main {
+    public static void main (String[] args) {
+        // variable dengan tipe data integer //
+        int sembilan = 9, sepuluh = 10, sama = sembilan+sepuluh;
+        // tipe data integer //
+        // nama variable sembilan & sepuluh //
+
+        // eksekusi //
+        System.out.println("Pertamban total "+sama);
+    }
+}
