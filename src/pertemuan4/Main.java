61f79bd0bd28e1d24be742d78f59e59f5b63510f
jaka junaedi <jaka.junaedi03@gmail.com>
2020-08-03 16:02:44 +0700

    pertemuan4 menganai aritmatika di java

---
+++ src/pertemuan4/Main.java
@@ -0,0 +1,21 @@
+package pertemuan4;
+
+public class Main {
+    public static void main (String args[]) {
+        int x = 32;
+        int y = 24;
+        int z = x+y;
+
+        // pengunakan karakter matematika //
+        int jumlah = x+y;
+        int kurang = x-y;
+        int kali = x*y;
+
+        System.out.println("X ditambah Y sama dengan "+z);
+
+        System.out.println(jumlah+" adalah hasil dari pengurangan");
+        System.out.println(kurang+" adalah hasil dari penambahan");
+        System.out.println(kali+" adalah hasil dari perkalian");
+
+    }
+}
