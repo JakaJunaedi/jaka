f842892c7ed1d54e1378d9b4f29b7ee6488790ce
jaka junaedi <jaka.junaedi03@gmail.com>
2020-08-03 16:41:06 +0700

    pertemuan6 penggunaan if & else

---
+++ src/pertemuan6/Main.java
@@ -0,0 +1,17 @@
+package pertemuan6;
+
+public class Main {
+    public static void main(String[] args) {
+        int budi = 1;
+            if(budi == 1) {
+                System.out.println("Jika Budi Sama dengan satu maka tampilkan");
+            }
+
+        int yes = 0;
+        if(yes == 1) {
+            System.out.println("Yes sama dengan 0");
+        }else{
+            System.out.println("bukan 0 makan tidak yes");
+        }
+    }
+}
