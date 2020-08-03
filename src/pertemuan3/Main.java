55ef58453b1815cacdfd0f3d7a86bef53df52870
jaka junaedi <jaka.junaedi03@gmail.com>
2020-08-03 15:24:44 +0700

    pertemuan3 belajar mengenai type data

---
+++ src/pertemuan3/Main.java
@@ -0,0 +1,18 @@
+package pertemuan3;
+
+public class Main {
+    public static void main(String[] pm) {
+        // char menggunakan tanda petik satu //
+        char angka = '1';
+        // interger tanpa menggunakan petik //
+        int nominal = 100;
+        // String sama dengan varchar menggunakn tanda petik 2 //
+        String huruf = "Hallo Sahabat";
+
+        // eksekusi //
+        System.out.println("###### Belajar Mengenai Type Data #####");
+        System.out.println(angka);
+        System.out.println(nominal);
+        System.out.println(huruf);
+    }
+}
